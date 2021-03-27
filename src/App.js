6dd23afe63dd0430d383c5d7f1877353ded02748
abc <omkargodham321@gmail.com>
2021-03-27 20:36:54 +0530

    use of mapStatesToProps and mapDispatchToProps for some values

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import { useDispatch, useSelector } from 'react-redux';
 
 import { decrementAction, incrementAction,decrementActionIce ,incrementActionIce, fetchUsers} from './actions/counterActions';
 
+ import {connect} from 'react-redux';
 
 
-function App() {
+function App(props) {
 
+    //accessing state using hooks 
     const count= useSelector(state => state.cake.count);
     const icecount = useSelector(state => state.iceCreame.icecount);
     const users = useSelector(state => state.users.users);
@@ -16,26 +18,32 @@ function App() {
     <div>
     <h1>Cakes</h1>
         <button onClick={() => dispatch(decrementAction())}> - </button>
-        <h1>{count}</h1>
-        <button onClick={() => dispatch(incrementAction())}> + </button>
+        <h1>{props.cakeCount}</h1>
+        <button onClick={ props.inCake}> + </button>
     </div>
     <div>
     <h1>Ice - Creams</h1>
         <button onClick={() => dispatch(decrementActionIce())}> - </button>
         <h1>{icecount}</h1>
-        <button onClick={() => dispatch(incrementActionIce())}> + </button>
+        <button onClick={()=> dispatch(incrementActionIce())}> + </button>
     </div>
     <div>
     <h1>Users</h1>
         <button onClick={() => dispatch(fetchUsers())}> GET USERS </button>
         {users.map(user => {
             return <p>{user.name}</p>
-        }) }
+        })}
     </div>
     </>
-
-
 }
 
+//accesssing state using these methods usin props
+    const mapStatesToProps = (state) =>({
+            cakeCount:state.cake.count
+        })
 
-export default App;
+     const mapDispatchToProps = (dispatch) => {
+     return  {   inCake : () => dispatch(incrementAction())}
+     }
+
+export default connect(mapStatesToProps,mapDispatchToProps)(App);
